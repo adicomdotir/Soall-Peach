85a03cccbd9d9513ec032497e1a47b1aa6b27eed
adicomdotir <adicom.ir@gmail.com>
2020-04-14 20:23:24 +0430

    MOdify prime logic

--- prime/Main.java
+++ prime/Main.java
@@ -23,8 +23,9 @@ public class Main {
 
 	private int isPrime(int num) {
         if (num <= 3) return 1;
+		if (num % 2 == 0) return 0;
 		int max = (int) Math.sqrt(num) + 1;
-        for (int i = 2; i <= max; i++) {
+        for (int i = 3; i <= max; i+=2) {
             if (num % i == 0) {
                 return 0;
             }
