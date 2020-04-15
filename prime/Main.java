768e8627345ca6ec8ce3d04f55f15525628a2fdd
Yashar Panahi <adicom.ir@gmail.com>
2020-04-15 08:59:50 +0430

    Modify output code

--- prime/Main.java
+++ prime/Main.java
@@ -3,33 +3,28 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
-		Main obj = new Main();
 		Scanner sc = null;
 		try {
 			sc = new Scanner(new File(args[0]));
+			OutputStream out = new BufferedOutputStream ( System.out );
+			while(sc.hasNext()) {
+				String num = sc.nextLine();
+				out.write((isPrime(Integer.parseInt(num)) + "\n").getBytes());
+			}
+			out.flush();
 		}
 		catch(Exception ex) {}
-
-		StringBuilder sb = new StringBuilder();
-		while(sc.hasNext()) {
-			String num = sc.nextLine();
-			sb.append(obj.isPrime(Integer.parseInt(num)));
-			sb.append("\n");
-		}
-		sb.setLength(sb.length() - 1);
-
-		System.out.println(sb.toString());
 	}
 
-	private int isPrime(int num) {
-        if (num <= 3) return 1;
+	private static int isPrime(int num) {
+        	if (num <= 3) return 1;
 		if (num % 2 == 0) return 0;
 		int max = (int) Math.sqrt(num) + 1;
-        for (int i = 3; i <= max; i+=2) {
-            if (num % i == 0) {
-                return 0;
-            }
-        }
-        return 1;
-    }
+		for (int i = 3; i <= max; i+=2) {
+		    if (num % i == 0) {
+			return 0;
+		    }
+		}
+		return 1;
+	}
 }
