be84573603400926f30b9ed8ac6463703cd6ccc1
adicomdotir <adicom.ir@gmail.com>
2020-04-16 20:05:31 +0430

    Modify input logic to stream

--- prime/Main.java
+++ prime/Main.java
@@ -1,18 +1,22 @@
 import java.util.*;
 import java.io.*;
+import java.util.stream.*;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		try {
-			String line = null;
-			FileReader in = new FileReader(new File(args[0]));
-			BufferedReader br = new BufferedReader(in);
-
+			Stream<String> lines = Files.lines(Paths.get(args[0]));
 			OutputStream out = new BufferedOutputStream ( System.out );
-
-			while ((line = br.readLine()) != null) {
-				out.write((isPrime(Integer.parseInt(line)) + "\n").getBytes());
-			}
+			lines.forEach(l -> {
+				try {
+					out.write((isPrime(Integer.parseInt(l)) + "\n").getBytes());
+				} catch(IOException e) {
+					e.printStackTrace();
+				} 
+			});
 			out.flush();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -20,10 +24,11 @@ public class Main {
 	}
 
 	private static int isPrime(int num) {
-        	if (num <= 3) return 1;
+        if (num <= 3) return 1;
 		if (num % 2 == 0) return 0;
+		if (num % 3 == 0) return 0;
 		int max = (int) Math.sqrt(num) + 1;
-		for (int i = 3; i <= max; i+=2) {
+		for (int i = 5; i <= max; i += 2) {
 		    if (num % i == 0) {
 			return 0;
 		    }
